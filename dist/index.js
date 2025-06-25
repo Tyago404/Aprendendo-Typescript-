6993e9fe940d60e8f6afc303a71e78c68417a64f
Tiago Santos <tiago40427@gmail.com>
2025-06-24 23:04:51 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const nome = 'Tiago';
+console.log(nome);
---
+++ src/index.ts
@@ -0,0 +1,316 @@
+//Tipos estáticos --------------
+    let greeting:string = "Hello, world!";
+    /*Quando declaramos uma let como string, não podemos alterar o valor dela para outro tipo */
+    //greeting = 32; //error
+    //greeting = 'tiago' //Correct
+
+    //temos vários tipos como: string, number, boolean e etc..
+//Tipos estáticos  --------------
+
+//Anotação de tipo  --------------
+    //Se criarmos uma variável sem tipo, o typescript usa 'Inferência de tipo' que atribui automaticamente o tipo
+        let price = 100
+        // price = "100" //Error
+
+    //O tipo 'Any' é um tipo de pode representar qualquer tipo, podendo até ser sobrescrevido 
+        let anything:any = 'What im?'
+        anything = 100 //Correct
+        //isso deve ser usado com moderação pos remove os benefícios da segurança de tipos
+//Anotação de tipo  --------------
+
+//Tipos de União  --------------
+    //Permite definir múltiplos tipos para uma variável
+    let variable: number | string | boolean | null | undefined
+    variable = 32
+    variable = true
+    variable = 'now im a string'
+//Tipos de União  --------------
+
+//Arrays  --------------
+    //Para definir um array precisamos indicar que essa variável irá armazenar um array do tipo dela
+    let numArray:number[] = [1,2,3,4,5]
+    let strArray:string[] = ['a','b','c','d']
+
+    //modificando um valor
+    numArray[4] = 20;
+    strArray.push('e')
+
+    //array de dois tipos
+    let stuff:(number|string)[] = [1,2,3,'a','c','d']
+//Arrays  --------------
+
+//Objetos  --------------
+//Declaramos igual ao Js porém primeiro precisamos tipar os atributos do objeto
+    let person: {
+        name:string,
+        last_name:string,
+        age:number,
+        scores: (number[] | string[]),
+    }
+    person = {
+        name:'Nadson',
+        last_name:'Ferinha',
+        age: 30,
+        scores: [1,2,3,4,5]
+    }
+    //Podemos interagir com os objetos
+    const personName:string = person.name
+    person.name = 'Tiago'
+//Objetos  --------------
+
+//Aliases de tipo  --------------
+    //Ou apelidos de tipo são para dar nomes aos tipos que nós mesmos criamos. Utilizamos a palavra-chave 'type'
+    type Human = {
+        name:string;  //ponto e vírgula
+        age:number;
+    }
+    //Utilizando nosso tipo criado
+    const nadson:Human = {name:'Nadson',age:30}
+
+    //Podemos criar um type para dar a um tipo existente um novo nome
+    type Prices = number[];
+    const precos:Prices = [2.50, 10.99, 11.99]
+
+    type UserId = number | string;
+    const id:UserId = "#222Ab3c" + 404
+//Aliases de tipo  --------------
+
+//Tipos de Interseção  --------------
+    //Permitem que você combine vários tipos em um único tipo  
+    type Person = {
+        name:string
+    }
+    type Employee = {
+        employeeId:number;
+    }
+    type PersonEmployee = Person & Employee; //usamos '&' para combinar os tipos
+
+    //para usar o tipo de interseção a variável deve satisfazer todos os tipos
+    const someone: PersonEmployee = {
+        name:'Nadson',
+        employeeId: 2469,
+    }
+
+    //Podemos fazer de uma forma mais simples
+    type Dog = {
+        name:string
+    }
+    type DogSituation = Dog & {
+        castrated:boolean
+    }
+    const junin:DogSituation = {name:'junin', castrated:true}
+//Tipos de Interseção  --------------
+
+//Interfaces  --------------
+    //Semelhante aos types, interfaces permite você definir tipos personalizados para objetos
+    interface Animal { //sem '=' 
+        name:string
+    }
+    //Funciona exatamente como o type
+    const pessoa:Animal = {name:'Nadson'}
+
+    //Utilizando a palavra-chave 'extends' podemos criar uma nova interface que herda uma existente
+    interface Pet extends Animal {
+        species:string;
+    }
+    const junio:Pet = {name:'junio', species:'stray'}
+//Interfaces  --------------
+
+//Propriedades Especiais  --------------
+    
+    //OPCIONAIS
+        //Quando utilizamos '?' significa que essa propriedade é opcional 
+        interface X {
+            firstName:string,
+            lastName:string,
+            middleName?:string, //opcional quando for utilizada em um objeto
+        }
+        const x:X = {
+            firstName:'Nadson',
+            lastName:'Ferinha',
+            //middleName é opcional e por isso não causa erro ao não ser declarado
+        }
+
+    //READONLY
+        //Quando utilizamos 'readonly' impede a modificação após a inicialização
+        type Character = {
+            readonly id:number; //Esse valor não pode ser mudado após ser declarado
+            name:string;
+        }
+
+        const joaozin:Character = {
+            id:1, //O valor foi declarado aqui e não poderá ser mudado mais
+            name:'Joaozin'
+        };
+            // joaozin.id = 3 //erro por conta do readonly
+//Propriedades Especiais  --------------
+
+//Funções  --------------
+        //Em ts devemos definir o tipo de retorno de uma função
+            function  greet():string{
+                return 'Hello, World!'
+            }
+                //console.log(greet())
+
+        //Quando não temos retorno utilizamos o tipo 'void'
+            function greet1():void{
+                console.log('Hello, World!')
+            }
+                //greet1()
+
+        //Quando passamos argumentos, precisamos também definir seus tipos
+        function greet2(name:string, last_name:string):void{
+            console.log(`Hello ${name} ${last_name}`)
+        }
+                //greet2('Nadson' , 'Ferinha')
+//Funções  --------------
+
+//Tipos de Funções e assinatura de funções  --------------
+    //Podemos declarar que uma variável pode conter uma função utilizando arrow (anotação de tipo, assinatura de função) 
+        let myFunction:(x:number,y:number)=>number;
+
+    //Agora podemos criar uma funçao a partir da nossa variável
+        myFunction = function(x:number, y:number):number{
+            return x + y
+        }
+    //armazenamos em uma variável o resultado  
+        const result = myFunction(5,5)
+        // console.log(result)
+
+    //Utilizando callback
+        type Callback = (data:string) => void; //tipo de função para callback
+
+        function processString(input:string, callback:Callback):void{
+            const processed = input.toUpperCase();
+            callback(processed)
+        }
+
+        const logResult:Callback = (data:string)=>{
+            console.log(data)
+        }
+        // processString("i'm hungry!",logResult) 
+
+//Tipos de Funções e assinatura de funções    --------------
+
+//Parâmetro Opcionais e Padrões  --------------
+    //Podemos tornar um parametro de uma função opcional veja:
+            function Hello(name:string, age?:number):void{
+                if(age !== undefined){
+                    console.log(`Hello ${name}, you are ${age} years old`)
+                }else{
+                    console.log(`Hello ${name}!`)
+                }
+            }
+            //Podemos chamar a função sem fornecer age
+                // Hello('Nadson') 
+            
+    //Podemos também adicionar um valor padrão ao parametro
+            function TotalPrice(price:number, taxRate:number=0.05):number{
+                return price + (price * taxRate)
+            }
+                //console.log(TotalPrice(60))
+                    //também podemos alterar o valor padrão: console.log(TotalPrice(60,00.8))
+//Parâmetro Opcionais e Padrões  --------------
+
+//Classes  --------------
+    //Utilizamos a palavra-chave 'Class' para criar uma classe
+            //Em seguida criamos um constructor 
+        
+        class People{
+        //precisamos tipar os parametros
+            name:string
+            constructor(name:string){ 
+                this.name = name
+            }
+        }
+        //Agora podemos instanciar a classe
+            const jaozao = new People('Jaozao')
+      
+    //PUBLIC PRIVATE PROTECTED -  Modificadores de acesso
+         //PUBLIC Torna a propriedade da classe publica e acessivel de fora, por padrão todas já são publicas
+         //PRIVATE Torna a propriedade da classe acessivel apenas dentro da classe
+
+         class PublicPrivate{
+            public data:string 
+            private id:number
+
+            constructor(data:string, id:number){
+                this.data = data
+                this.id = id
+            }
+         }
+         const test = new PublicPrivate('DataContent', 123)
+            // console.log(test.id) //Erro pois a propriedade id só é acessada dentro da classe pois é privada
+         
+        
+        //PROTECTED  Torna a propriedade da classe acessivel apenas dentro da classe e de suas subclasses 
+        class Employeee {
+            protected departament:string;
+            constructor(departament:string){
+                this.departament = departament
+            }
+        }
+
+        class Manager extends Employeee{
+            printDepartament(){
+                console.log(this.departament)
+            }
+        }
+        const manager = new Manager('Sales')
+       // console.log(manager.printDepartament()) //é acessivel até aqui
+//Classes  --------------
+
+//Classes abstratas  --------------
+    //São classes que não podem ser instanciadas diretamente
+        abstract class X{
+        }
+        // const y = new X() //ERROR
+
+    //Também temos metodos abstratos
+        abstract class Pet1 {
+          public abstract makeNoise():void //geralmente podemos combinar modificadores de acesso  
+        }
+    //Para utilizar essa classe precisamos extender elas
+
+    class Cat extends Pet1 {
+        makeNoise(): void {
+            console.log('MEOW')
+        }
+    }
+    const gatin = new Cat()
+       // gatin.makeNoise()
+//Classes abstratas  --------------
+
+//Enums  --------------
+    //São um conjunto de constantes nomeadas, iniciamos com a palavra-chave 'Enum'
+        //Definimos um tipo e em seguida os valores separados por ,
+            enum Directions{
+                Up,
+                Down,
+                Left,
+                Right
+            }
+
+    //Cada numero tem um index, iniciando com 0
+        // console.log(Directions.Up) // === 0 
+    
+    //Podemos definir manualmente o valor dos membros do enum, fazemos com o primeiro e os demais seguirão a ordem
+            enum Directions2{
+                Up = 5,
+                Down, //valor 6
+                Left, //valor 7
+                Right, //valor 8
+            }
+            //Para acessar acessar um membro do enum
+                console.log(Directions2[5]) // === Up
+
+
+
+//PT2
+    //Podemos usar valores de string também
+            enum MusicGenres{
+                ThrashMetal = 'THRASHMETAL',
+                Metal = 'METAL',
+                BlackMetal = 'BLACKMETAL'
+            }
+//Enums  --------------
